ca32c504afb47522ff7980bd9b74156cbdac3186
Justin Biard <icodealot@gmail.com>
2022-10-08 12:08:42 -0500

    get provider from auth

--- src/oci/auth.clj
+++ src/oci/auth.clj
@@ -1,14 +1,28 @@
 (ns oci.auth
-  (:import (com.oracle.bmc ConfigFileReader)))
+  (:import (com.oracle.bmc ConfigFileReader)
+           (com.oracle.bmc.auth ConfigFileAuthenticationDetailsProvider)))
+
+(def default "DEFAULT")
 
 (defn get-config 
   ([]
    ;; no configuration profile specified calling get-config
    ;; i.e. `(get-config)` so try to get the DEFAULT profile
-   (get-config "DEFAULT"))
+   (get-config default))
   ([profile]
    ;; configuration profile specified so attempt to use that
    ;; unless it is falsy in which case, look for DEFAULT
+   ;; ConfigFileReader.parseDefault also works with no param
+   ;; but adding default here for symmetry
   (if profile
     (ConfigFileReader/parseDefault profile)
-    (ConfigFileReader/parseDefault))))
+    (ConfigFileReader/parseDefault default))))
+
+(defn get-provider
+  ([]
+   ;; no profile specified so calling get-provider with DEFAULT
+   (get-provider default))
+  ([profile]
+   (if profile
+     (new ConfigFileAuthenticationDetailsProvider profile)
+     (new ConfigFileAuthenticationDetailsProvider default))))
--- test/oci/auth_test.clj
+++ test/oci/auth_test.clj
@@ -2,22 +2,30 @@
   (:require [clojure.test :refer :all] 
             [oci.auth :refer :all]))
 
-(def profile "DEFAULT")
-
 (deftest config-not-nil-test
   (testing "config should not be nil or false"
-    (is (get-config "DEFAULT"))
+    (is (get-config default))
     (is (get-config))))
 
-(deftest implicit-default-matches-default-test
+(deftest implicit-default-matches-explicit-test
   (testing "implicit default config should equal explicit" 
     (is (= (.get (get-config) "user")
-           (.get (get-config profile) "user")))
+           (.get (get-config default) "user")))
     (is (= (.get (get-config) "fingerprint")
-           (.get (get-config profile) "fingerprint")))
+           (.get (get-config default) "fingerprint")))
     (is (= (.get (get-config) "tenancy")
-           (.get (get-config profile) "tenancy")))
-    (is (= (.get (get-config) "region")
-           (.get (get-config profile) "region")))
-    (is (= (.get (get-config) "key_file")
-           (.get (get-config profile) "key_file")))))
+           (.get (get-config default) "tenancy")))))
+
+(deftest provider-not-nil-test
+  (testing "provider should not be nil or false"
+    (is (get-provider default))
+    (is (get-provider))))
+
+(deftest provider-region-matches-config-test
+  (testing "provider should match default config"
+    (is (= (.get (get-config) "user")
+           (.getUserId (get-provider))))
+    (is (= (.get (get-config) "fingerprint")
+           (.getFingerprint (get-provider))))
+    (is (= (.get (get-config) "tenancy")
+           (.getTenantId (get-provider))))))
