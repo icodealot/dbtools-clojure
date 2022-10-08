8e0e1f403bc360cfbb641c2d5b051061e22c58b6
Justin Biard <icodealot@gmail.com>
2022-10-08 09:53:23 -0500

    initial setup

---
+++ test/dbtools_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns dbtools-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [dbtools-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
