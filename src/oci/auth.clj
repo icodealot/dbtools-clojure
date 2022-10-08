7b4f24e5e6cdbe7df5c7da2c50c59175b1eed344
Justin Biard <icodealot@gmail.com>
2022-10-08 11:42:42 -0500

    config with or without a profile

--- src/oci/auth.clj
+++ src/oci/auth.clj
@@ -2,7 +2,13 @@
   (:import (com.oracle.bmc ConfigFileReader)))
 
 (defn get-config 
-  [profile]
+  ([]
+   ;; no configuration profile specified calling get-config
+   ;; i.e. `(get-config)` so try to get the DEFAULT profile
+   (get-config "DEFAULT"))
+  ([profile]
+   ;; configuration profile specified so attempt to use that
+   ;; unless it is falsy in which case, look for DEFAULT
   (if profile
     (ConfigFileReader/parseDefault profile)
-    (ConfigFileReader/parseDefault)))
+    (ConfigFileReader/parseDefault))))
--- test/oci/auth_test.clj
+++ test/oci/auth_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all] 
             [oci.auth :refer :all]))
 
+(def profile "DEFAULT")
+
 (deftest config-not-nil-test
   (testing "config should not be nil or false"
-    (is (get-config "DEFAULT"))))
+    (is (get-config "DEFAULT"))
+    (is (get-config))))
+
+(deftest implicit-default-matches-default-test
+  (testing "implicit default config should equal explicit" 
+    (is (= (.get (get-config) "user")
+           (.get (get-config profile) "user")))
+    (is (= (.get (get-config) "fingerprint")
+           (.get (get-config profile) "fingerprint")))
+    (is (= (.get (get-config) "tenancy")
+           (.get (get-config profile) "tenancy")))
+    (is (= (.get (get-config) "region")
+           (.get (get-config profile) "region")))
+    (is (= (.get (get-config) "key_file")
+           (.get (get-config profile) "key_file")))))
